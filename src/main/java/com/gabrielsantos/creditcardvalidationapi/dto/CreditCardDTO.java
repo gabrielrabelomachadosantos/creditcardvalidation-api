acb6b4798553d313321169132fca73d0d34724e3
Gabriel Rabelo Machado Santos <gabrielramasa@gmail.com>
2022-03-30 20:40:31 -0300

    --Changing API models

--- src/main/java/com/gabrielsantos/creditcardvalidationapi/dto/CreditCardDTO.java
+++ src/main/java/com/gabrielsantos/creditcardvalidationapi/dto/CreditCardDTO.java
@@ -17,11 +17,11 @@ import java.util.Date;
 @AllArgsConstructor
 public class CreditCardDTO {
 
-    @ApiModelProperty(example = "9999 9999 9999 9999 or 9999999999999999")
+    @ApiModelProperty(example = "5245 8364 2920 5934 or 5245836429205934")
     private String number;
 
     @JsonFormat(pattern = "yyyy/MM")
-    @ApiModelProperty(example = "yyyy/MM")
+    @ApiModelProperty(example = "2030/12")
     private Date expiringDate;
 
     @JsonIgnore
